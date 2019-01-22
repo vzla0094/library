5c077765581276b232e813be65be375256769510
vzla0094 <vzla0094@gmail.com>
2019-01-22 16:09:21 -0700

    added 'change status' button and functionality

--- practica.js
+++ practica.js
@@ -8,26 +8,41 @@ function nodeMaker(nodeType, parent, text){
     let newNode = document.createElement(nodeType);
     if (!undefined) newNode.textContent = text;
     parent.appendChild(newNode);
+    return newNode;
 }
 
 function Book(author, title, numPages, state){
     this.Author = author;
     this.Title = title;
-    this['Number of Pages'] = numPages;
+    this.Pages = numPages;
     this.Read = state;
 }
 
 Book.prototype.render = function(){
-    let cardContainer = document.createElement('div');
-    for (let key in this){
-        if(this.hasOwnProperty(key)){
-            nodeMaker('p', cardContainer, `${key}: ${this[key]}`);        
-        }
+    this.cardContainer = document.createElement('div');
+    this.cardContainer.setAttribute('data-title', `${this.Title}`);
+    this.DOMTitle = nodeMaker('p', this.cardContainer, `Title: ${this.Title}`);
+    this.DOMAuthor = nodeMaker('p', this.cardContainer, `Author: ${this.Author}`);
+    this.DOMPages = nodeMaker('p', this.cardContainer, `Number of pages: ${this.Pages}`);
+    this.DOMRead = nodeMaker('p', this.cardContainer, `Read: ${this.Read}`);
+    nodeMaker('button', this.cardContainer, 'Delete')
+        .setAttribute('data-action', 'delete');
+    nodeMaker('button', this.cardContainer, 'Change read status')
+        .setAttribute('data-action', 'state');
+    this.cardContainer.querySelector('button[data-action="delete"]')
+        .addEventListener('click', wipe);
+    this.cardContainer.querySelector('button[data-action="state"]')
+        .addEventListener('click', callChangeState);
+    document.body.appendChild(this.cardContainer);
+}
+
+Book.prototype.changeState = function(){
+    if(this.Read === 'Yes'){
+        this.Read = 'No';
+    }else{
+        this.Read = 'Yes';
     }
-    nodeMaker('button', cardContainer, 'Delete');
-    cardContainer.querySelector('button').addEventListener('click', wipe);
-    cardContainer.setAttribute('data-index', `${myLibrary.indexOf(this)}`);
-    document.body.appendChild(cardContainer);
+    this.DOMRead.textContent = `Read: ${this.Read}`;
 }
 
 function makeBook(event){
@@ -42,6 +57,16 @@ function makeBook(event){
 }
 
 function wipe (){
-    myLibrary.splice(parseInt(this.parentNode.getAttribute('data-index')), 1);
+    let obj = myLibrary.find(elem => {
+        return elem.cardContainer === this.parentNode;
+    }, this)
+    myLibrary.splice(myLibrary.indexOf(obj),1);
     this.parentNode.remove();
+}
+
+function callChangeState(){
+    let obj = myLibrary.find(elem => {
+        return elem.cardContainer === this.parentNode;
+    }, this)
+    obj.changeState();
 }
